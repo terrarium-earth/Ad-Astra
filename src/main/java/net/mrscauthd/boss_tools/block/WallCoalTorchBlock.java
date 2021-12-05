e4da5394683948c623fcfbcad825ded0ce9d5d24
MrScautHD <MrScautHD5@gmail.com>
2021-12-05 09:05:42 +0100

    Update WallCoalTorchBlock.java

--- src/main/java/net/mrscauthd/boss_tools/block/WallCoalTorchBlock.java
+++ src/main/java/net/mrscauthd/boss_tools/block/WallCoalTorchBlock.java
@@ -2,80 +2,77 @@ package net.mrscauthd.boss_tools.block;
 
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Maps;
-import java.util.Map;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.block.*;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
+
 import javax.annotation.Nullable;
-
-import net.minecraft.block.*;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.BlockItemUseContext;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
-import net.minecraft.particles.IParticleData;
-import net.minecraft.state.DirectionProperty;
-import net.minecraft.state.EnumProperty;
-import net.minecraft.state.StateContainer;
-import net.minecraft.util.*;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.BlockRayTraceResult;
-import net.minecraft.util.math.shapes.ISelectionContext;
-import net.minecraft.util.math.shapes.VoxelShape;
-import net.minecraft.util.registry.Registry;
-import net.minecraft.world.IBlockReader;
-import net.minecraft.world.IWorld;
-import net.minecraft.world.IWorldReader;
-import net.minecraft.world.World;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.mrscauthd.boss_tools.block.CoalTorchBlock;
-
-import DirectionProperty;
+import java.util.Map;
 
 public class WallCoalTorchBlock extends CoalTorchBlock {
-	public static final DirectionProperty HORIZONTAL_FACING = HorizontalBlock.HORIZONTAL_FACING;
-	private static final Map<Direction, VoxelShape> SHAPES = Maps.newEnumMap(ImmutableMap.of(Direction.NORTH, Block.makeCuboidShape(5.5D, 3.0D, 11.0D, 10.5D, 13.0D, 16.0D), Direction.SOUTH, Block.makeCuboidShape(5.5D, 3.0D, 0.0D, 10.5D, 13.0D, 5.0D), Direction.WEST, Block.makeCuboidShape(11.0D, 3.0D, 5.5D, 16.0D, 13.0D, 10.5D), Direction.EAST, Block.makeCuboidShape(0.0D, 3.0D, 5.5D, 5.0D, 13.0D, 10.5D)));
+	public static final DirectionProperty HORIZONTAL_FACING = HorizontalDirectionalBlock.FACING;
+	private static final Map<Direction, VoxelShape> SHAPES = Maps.newEnumMap(ImmutableMap.of(Direction.NORTH, Block.box(5.5D, 3.0D, 11.0D, 10.5D, 13.0D, 16.0D), Direction.SOUTH, Block.box(5.5D, 3.0D, 0.0D, 10.5D, 13.0D, 5.0D), Direction.WEST, Block.box(11.0D, 3.0D, 5.5D, 16.0D, 13.0D, 10.5D), Direction.EAST, Block.box(0.0D, 3.0D, 5.5D, 5.0D, 13.0D, 10.5D)));
 
-	public WallCoalTorchBlock(AbstractBlock.Properties properties) {
+	public WallCoalTorchBlock(BlockBehaviour.Properties properties) {
 		super(properties);
-		this.setDefaultState(this.stateContainer.getBaseState().with(HORIZONTAL_FACING, Direction.NORTH));
+		this.registerDefaultState(this.stateDefinition.any().setValue(HORIZONTAL_FACING, Direction.NORTH));
 	}
 
 	@Override
-	public ActionResultType onBlockActivated(BlockState state, World world, BlockPos pos, PlayerEntity entity, Hand hand, BlockRayTraceResult hit) {
-		super.onBlockActivated(state, world, pos, entity, hand, hit);
-		return ActionResultType.SUCCESS;
+	public InteractionResult use(BlockState p_60503_, Level p_60504_, BlockPos p_60505_, Player p_60506_, InteractionHand p_60507_, BlockHitResult p_60508_) {
+		return super.use(p_60503_, p_60504_, p_60505_, p_60506_, p_60507_, p_60508_);
 	}
 
-	public String getTranslationKey() {
-		return this.asItem().getTranslationKey();
+	@Override
+	public String getDescriptionId() {
+		return this.asItem().getDescriptionId();
 	}
 
-	public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
-		return getShapeForState(state);
+	@Override
+	public VoxelShape getShape(BlockState p_60555_, BlockGetter p_60556_, BlockPos p_60557_, CollisionContext p_60558_) {
+		return getShapeForState(p_60555_);
 	}
 
 	public static VoxelShape getShapeForState(BlockState state) {
-		return SHAPES.get(state.get(HORIZONTAL_FACING));
+		return SHAPES.get(state.getValue(HORIZONTAL_FACING));
 	}
 
-	public boolean isValidPosition(BlockState state, IWorldReader worldIn, BlockPos pos) {
-		Direction direction = state.get(HORIZONTAL_FACING);
-		BlockPos blockpos = pos.offset(direction.getOpposite());
-		BlockState blockstate = worldIn.getBlockState(blockpos);
-		return blockstate.isSolidSide(worldIn, blockpos, direction);
+	@Override
+	public boolean canSurvive(BlockState p_58133_, LevelReader p_58134_, BlockPos p_58135_) {
+		Direction direction = p_58133_.getValue(HORIZONTAL_FACING);
+		BlockPos blockpos = p_58135_.relative(direction.getOpposite());
+		BlockState blockstate = p_58134_.getBlockState(blockpos);
+		return blockstate.isFaceSturdy(p_58134_, blockpos, direction);
 	}
 
 	@Nullable
-	public BlockState getStateForPlacement(BlockItemUseContext context) {
-		BlockState blockstate = this.getDefaultState();
-		IWorldReader iworldreader = context.getWorld();
-		BlockPos blockpos = context.getPos();
+	@Override
+	public BlockState getStateForPlacement(BlockPlaceContext context) {
+		BlockState blockstate = this.defaultBlockState();
+		LevelAccessor iworldreader = context.getLevel();
+		BlockPos blockpos = context.getClickedPos();
 		Direction[] adirection = context.getNearestLookingDirections();
 
 		for(Direction direction : adirection) {
 			if (direction.getAxis().isHorizontal()) {
 				Direction direction1 = direction.getOpposite();
-				blockstate = blockstate.with(HORIZONTAL_FACING, direction1);
-				if (blockstate.isValidPosition(iworldreader, blockpos)) {
+				blockstate = blockstate.setValue(HORIZONTAL_FACING, direction1);
+				if (blockstate.canSurvive(iworldreader, blockpos)) {
 					return blockstate;
 				}
 			}
@@ -84,19 +81,23 @@ public class WallCoalTorchBlock extends CoalTorchBlock {
 		return null;
 	}
 
-	public BlockState updatePostPlacement(BlockState stateIn, Direction facing, BlockState facingState, IWorld worldIn, BlockPos currentPos, BlockPos facingPos) {
-		return facing.getOpposite() == stateIn.get(HORIZONTAL_FACING) && !stateIn.isValidPosition(worldIn, currentPos) ? Blocks.AIR.getDefaultState() : stateIn;
+	@Override
+	public BlockState updateShape(BlockState stateIn, Direction facing, BlockState facingState, LevelAccessor worldIn, BlockPos currentPos, BlockPos facingPos) {
+		return facing.getOpposite() == stateIn.getValue(HORIZONTAL_FACING) && !stateIn.canSurvive(worldIn, currentPos) ? Blocks.AIR.defaultBlockState() : stateIn;
 	}
 
+	@Override
 	public BlockState rotate(BlockState state, Rotation rot) {
-		return state.with(HORIZONTAL_FACING, rot.rotate(state.get(HORIZONTAL_FACING)));
+		return state.setValue(HORIZONTAL_FACING, rot.rotate(state.getValue(HORIZONTAL_FACING)));
 	}
 
-	public BlockState mirror(BlockState state, Mirror mirrorIn) {
-		return state.rotate(mirrorIn.toRotation(state.get(HORIZONTAL_FACING)));
+	@Override
+	public BlockState mirror(BlockState p_60528_, Mirror p_60529_) {
+		return p_60528_.rotate(p_60529_.getRotation(p_60528_.getValue(HORIZONTAL_FACING)));
 	}
 
-	protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
-		builder.add(HORIZONTAL_FACING);
+	@Override
+	protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> p_49915_) {
+		p_49915_.add(HORIZONTAL_FACING);
 	}
 }
