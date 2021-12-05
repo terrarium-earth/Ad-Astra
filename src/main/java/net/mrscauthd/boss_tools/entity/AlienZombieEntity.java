c2395108a6f970bded7dc09b7f7ace78f87fa5cb
MrScautHD <MrScautHD5@gmail.com>
2021-12-05 09:20:16 +0100

    Update AlienZombieEntity.java

--- src/main/java/net/mrscauthd/boss_tools/entity/AlienZombieEntity.java
+++ src/main/java/net/mrscauthd/boss_tools/entity/AlienZombieEntity.java
@@ -1,38 +1,46 @@
 package net.mrscauthd.boss_tools.entity;
 
-import net.minecraft.entity.*;
-import net.minecraft.entity.ai.attributes.AttributeModifierMap;
-import net.minecraft.entity.ai.attributes.Attributes;
-import net.minecraft.entity.ai.goal.*;
-import net.minecraft.util.SoundEvent;
+import net.minecraft.network.protocol.Packet;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.MobType;
+import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.ai.goal.MeleeAttackGoal;
+import net.minecraft.world.entity.ai.goal.RandomLookAroundGoal;
+import net.minecraft.world.entity.ai.goal.RandomStrollGoal;
+import net.minecraft.world.entity.ai.goal.RangedAttackGoal;
+import net.minecraft.world.entity.ai.goal.target.HurtByTargetGoal;
+import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
+import net.minecraft.world.entity.monster.Monster;
+import net.minecraft.world.entity.monster.RangedAttackMob;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.network.NetworkHooks;
 import net.mrscauthd.boss_tools.entity.alien.AlienEntity;
 import net.mrscauthd.boss_tools.events.Config;
 
 import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.fml.network.NetworkHooks;
 
-import net.minecraft.world.World;
-import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.DamageSource;
-import net.minecraft.network.IPacket;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.entity.monster.MonsterEntity;
-
-public class AlienZombieEntity extends MonsterEntity implements IRangedAttackMob {
-	public AlienZombieEntity(EntityType type, World world) {
+public class AlienZombieEntity extends Monster implements RangedAttackMob {
+	public AlienZombieEntity(EntityType type, Level world) {
 		super(type, world);
-		this.experienceValue = 5;
+		this.xpReward = 5;
 	}
 
-	public static AttributeModifierMap.MutableAttribute setCustomAttributes() {
-		return MobEntity.func_233666_p_()
-		.createMutableAttribute(Attributes.MOVEMENT_SPEED, 0.3)
-		.createMutableAttribute(Attributes.MAX_HEALTH, 20)
-		.createMutableAttribute(Attributes.ATTACK_DAMAGE, 3);
+	public static AttributeSupplier.Builder setCustomAttributes() {
+		return Mob.createMobAttributes()
+		.add(Attributes.MOVEMENT_SPEED, 0.3)
+		.add(Attributes.MAX_HEALTH, 20)
+		.add(Attributes.ATTACK_DAMAGE, 3);
 	}
 
 	@Override
-	public IPacket<?> createSpawnPacket() {
+	public Packet<?> getAddEntityPacket() {
 		return NetworkHooks.getEntitySpawningPacket(this);
 	}
 
@@ -41,25 +49,25 @@ public class AlienZombieEntity extends MonsterEntity implements IRangedAttackMob
 		super.registerGoals();
 		this.goalSelector.addGoal(1, new MeleeAttackGoal(this, 1.2, false));
 		this.targetSelector.addGoal(2, new HurtByTargetGoal(this));
-		this.goalSelector.addGoal(3, new RandomWalkingGoal(this, 0.8));
-		this.goalSelector.addGoal(4, new LookRandomlyGoal(this));
-		this.targetSelector.addGoal(5, new NearestAttackableTargetGoal(this, PlayerEntity.class, false, false));
+		this.goalSelector.addGoal(3, new RandomStrollGoal(this, 0.8));
+		this.goalSelector.addGoal(4, new RandomLookAroundGoal(this));
+		this.targetSelector.addGoal(5, new NearestAttackableTargetGoal(this, Player.class, false, false));
 		this.targetSelector.addGoal(6, new NearestAttackableTargetGoal(this, AlienEntity.class, false, false));
 		this.goalSelector.addGoal(1, new RangedAttackGoal(this, 1.25, 20, 15) {
 			@Override
-			public boolean shouldContinueExecuting() {
-				return this.shouldExecute();
+			public boolean canContinueToUse() {
+				return this.canUse();
 			}
 		});
 	}
 
 	@Override
-	public CreatureAttribute getCreatureAttribute() {
-		return CreatureAttribute.UNDEAD;
+	public MobType getMobType() {
+		return MobType.UNDEAD;
 	}
 
 	@Override
-	public SoundEvent getHurtSound(DamageSource ds) {
+	protected SoundEvent getHurtSound(DamageSource p_33034_) {
 		return ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("entity.pillager.hurt"));
 	}
 
@@ -69,16 +77,16 @@ public class AlienZombieEntity extends MonsterEntity implements IRangedAttackMob
 	}
 
 	@Override
-	public void attackEntityWithRangedAttack(LivingEntity target, float flval) {
-		AlienSpitEntity.shoot(this, target, 2);
+	public void performRangedAttack(LivingEntity p_33317_, float p_33318_) {
+		AlienSpitEntity.shoot(this, p_33317_, 2);
 	}
 
 	@Override
 	public void tick() {
 		super.tick();
 		if (!Config.AlienZombieSpawn) {
-			if (!this.world.isRemote()) {
-				this.remove();
+			if (!this.level.isClientSide) {
+				this.remove(RemovalReason.DISCARDED);
 			}
 		}
 	}
