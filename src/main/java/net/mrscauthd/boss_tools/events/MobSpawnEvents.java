920b2f9b85ab99d8779d8ebd1812c5f4c349bf52
MrScautHD <MrScautHD5@gmail.com>
2021-12-08 15:44:24 +0100

    Added Moon Generation (other worlds still bugy)

--- src/main/java/net/mrscauthd/boss_tools/events/MobSpawnEvents.java
+++ src/main/java/net/mrscauthd/boss_tools/events/MobSpawnEvents.java
@@ -15,8 +15,8 @@ public class MobSpawnEvents {
     @SubscribeEvent
     public static void addLivingEntityToBiomes(BiomeLoadingEvent event) {
         if (event.getName().equals(new ResourceLocation(BossToolsMod.ModId, "moon"))) {
-            event.getSpawns().getSpawner(MobCategory.create("monster", "monster", 2, false , false, 128)).add(new MobSpawnSettings.SpawnerData(ModInnet.ALIEN_ZOMBIE.get(), 1, 1, 1));
-            event.getSpawns().getSpawner(MobCategory.create("monster", "monster", 4, false , false, 128)).add(new MobSpawnSettings.SpawnerData(ModInnet.STAR_CRAWLER.get(), 1, 1, 1));
+        //    event.getSpawns().getSpawner(MobCategory.create("monster", "monster", 2, false , false, 128)).add(new MobSpawnSettings.SpawnerData(ModInnet.ALIEN_ZOMBIE.get(), 1, 1, 1));
+        //    event.getSpawns().getSpawner(MobCategory.create("monster", "monster", 4, false , false, 128)).add(new MobSpawnSettings.SpawnerData(ModInnet.STAR_CRAWLER.get(), 1, 1, 1));
         }
     }
 }
