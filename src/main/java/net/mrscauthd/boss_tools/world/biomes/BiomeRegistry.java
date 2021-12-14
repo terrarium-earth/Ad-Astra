19f87a1128fec4ea48edfd87988202ee8939e526
MrScautHD <MrScautHD5@gmail.com>
2021-12-14 13:53:31 +0100

    Deleted Mercury Magma Biome

--- src/main/java/net/mrscauthd/boss_tools/world/biomes/BiomeRegistry.java
+++ src/main/java/net/mrscauthd/boss_tools/world/biomes/BiomeRegistry.java
@@ -26,7 +26,6 @@ public class BiomeRegistry {
     public static Biome infernal_venus_barrens;
 
     public static Biome mercury;
-    public static Biome mercury_magma;
 
     public static Biome orbit;
 
@@ -67,7 +66,7 @@ public class BiomeRegistry {
         if (venus == null) {
             BiomeSpecialEffects effects = new BiomeSpecialEffects.Builder().fogColor(-3044526).waterColor(4159204).waterFogColor(329011).skyColor(-3044526).foliageColorOverride(7842607).grassColorOverride(9551193).ambientParticle(new AmbientParticleSettings(ParticleTypes.CRIMSON_SPORE, 0.02f)).build();
             BiomeGenerationSettings.Builder biomeGenerationSettings = new BiomeGenerationSettings.Builder();
-            addDefaultCarversAndLakesLava(biomeGenerationSettings);
+            addDefaultCarversAndLakes(biomeGenerationSettings);
             MobSpawnSettings mobSpawnInfo = MobSpawnSettings.EMPTY;
 
             venus = new Biome.BiomeBuilder().precipitation(Biome.Precipitation.RAIN).biomeCategory(Biome.BiomeCategory.NONE).mobSpawnSettings(mobSpawnInfo).temperature(1.5f).downfall(1f).specialEffects(effects).generationSettings(biomeGenerationSettings.build()).build();
@@ -78,7 +77,7 @@ public class BiomeRegistry {
             BiomeSpecialEffects effects = new BiomeSpecialEffects.Builder().fogColor(-3044526).waterColor(4159204).waterFogColor(329011).skyColor(-3044526).foliageColorOverride(7842607).grassColorOverride(9551193).ambientParticle(new AmbientParticleSettings(ParticleTypes.CRIMSON_SPORE, 0.02f)).build();
             BiomeGenerationSettings.Builder biomeGenerationSettings = new BiomeGenerationSettings.Builder();
             //biomeGenerationSettings.surfaceBuilder(ConfiguredSurfaceBuilders.field_244170_b);  //Todo need to add the Layer in the world
-            addDefaultCarversAndLakesLava(biomeGenerationSettings);
+            addDefaultCarversAndLakes(biomeGenerationSettings);
             biomeGenerationSettings.addFeature(GenerationStep.Decoration.SURFACE_STRUCTURES, NetherPlacements.DELTA);
             biomeGenerationSettings.addFeature(GenerationStep.Decoration.VEGETAL_DECORATION, NetherPlacements.SMALL_BASALT_COLUMNS);
             biomeGenerationSettings.addFeature(GenerationStep.Decoration.UNDERGROUND_DECORATION, NetherPlacements.BASALT_BLOBS);
@@ -97,7 +96,7 @@ public class BiomeRegistry {
         if (venus_hills == null) {
             BiomeSpecialEffects effects = new BiomeSpecialEffects.Builder().fogColor(-3044526).waterColor(4159204).waterFogColor(329011).skyColor(-3044526).foliageColorOverride(7842607).grassColorOverride(9551193).ambientParticle(new AmbientParticleSettings(ParticleTypes.CRIMSON_SPORE, 0.02f)).build();
             BiomeGenerationSettings.Builder biomeGenerationSettings = new BiomeGenerationSettings.Builder();
-            addDefaultCarversAndLakesLava(biomeGenerationSettings);
+            addDefaultCarversAndLakes(biomeGenerationSettings);
             MobSpawnSettings mobSpawnInfo = MobSpawnSettings.EMPTY;
 
             venus_hills = new Biome.BiomeBuilder().precipitation(Biome.Precipitation.RAIN).biomeCategory(Biome.BiomeCategory.NONE).mobSpawnSettings(mobSpawnInfo).temperature(1.5f).downfall(1f).specialEffects(effects).generationSettings(biomeGenerationSettings.build()).build();
@@ -107,20 +106,9 @@ public class BiomeRegistry {
         if (mercury == null) {
             BiomeSpecialEffects effects = new BiomeSpecialEffects.Builder().fogColor(-16777216).waterColor(4159204).waterFogColor(329011).skyColor(-16777216).foliageColorOverride(7842607).grassColorOverride(9551193).build();
             BiomeGenerationSettings.Builder biomeGenerationSettings = new BiomeGenerationSettings.Builder();
-            addDefaultCarversAndLakesLava(biomeGenerationSettings);
-            MobSpawnSettings mobSpawnInfo = MobSpawnSettings.EMPTY;
-
-            mercury = new Biome.BiomeBuilder().precipitation(Biome.Precipitation.NONE).biomeCategory(Biome.BiomeCategory.NONE).mobSpawnSettings(mobSpawnInfo).temperature(1.6f).downfall(0f).specialEffects(effects).generationSettings(biomeGenerationSettings.build()).build();
-            event.getRegistry().register(mercury.setRegistryName(BossToolsMod.ModId,"mercury"));
-        }
-
-        if (mercury_magma == null) {
-            BiomeSpecialEffects effects = new BiomeSpecialEffects.Builder().fogColor(-16777216).waterColor(4159204).waterFogColor(329011).skyColor(-16777216).foliageColorOverride(7842607).grassColorOverride(9551193).build();
-            BiomeGenerationSettings.Builder biomeGenerationSettings = new BiomeGenerationSettings.Builder();
-            //biomeGenerationSettings.surfaceBuilder(ConfiguredSurfaceBuilders.field_244170_b);
-            addDefaultCarversAndLakesLava(biomeGenerationSettings);
+            addDefaultCarversAndLakes(biomeGenerationSettings);
             biomeGenerationSettings.addFeature(GenerationStep.Decoration.SURFACE_STRUCTURES, NetherPlacements.DELTA);
-            //biomeGenerationSettings.addFeature(GenerationStep.Decoration.VEGETAL_DECORATION, NetherPlacements.SMALL_BASALT_COLUMNS);
+            biomeGenerationSettings.addFeature(GenerationStep.Decoration.UNDERGROUND_DECORATION, NetherPlacements.BASALT_PILLAR);
             biomeGenerationSettings.addFeature(GenerationStep.Decoration.UNDERGROUND_DECORATION, NetherPlacements.BASALT_BLOBS);
             biomeGenerationSettings.addFeature(GenerationStep.Decoration.UNDERGROUND_DECORATION, NetherPlacements.BLACKSTONE_BLOBS);
             biomeGenerationSettings.addFeature(GenerationStep.Decoration.UNDERGROUND_DECORATION, NetherPlacements.SPRING_DELTA);
@@ -130,8 +118,8 @@ public class BiomeRegistry {
             biomeGenerationSettings.addFeature(GenerationStep.Decoration.UNDERGROUND_DECORATION, NetherPlacements.SPRING_CLOSED_DOUBLE);
             MobSpawnSettings mobSpawnInfo = MobSpawnSettings.EMPTY;
 
-            mercury_magma = new Biome.BiomeBuilder().precipitation(Biome.Precipitation.NONE).biomeCategory(Biome.BiomeCategory.NONE).mobSpawnSettings(mobSpawnInfo).temperature(1.6f).downfall(0f).specialEffects(effects).generationSettings(biomeGenerationSettings.build()).build();
-            event.getRegistry().register(mercury_magma.setRegistryName(BossToolsMod.ModId,"mercury_magma"));
+            mercury = new Biome.BiomeBuilder().precipitation(Biome.Precipitation.NONE).biomeCategory(Biome.BiomeCategory.NONE).mobSpawnSettings(mobSpawnInfo).temperature(1.6f).downfall(0f).specialEffects(effects).generationSettings(biomeGenerationSettings.build()).build();
+            event.getRegistry().register(mercury.setRegistryName(BossToolsMod.ModId,"mercury"));
         }
 
         if (orbit == null) {
@@ -142,13 +130,6 @@ public class BiomeRegistry {
             orbit = new Biome.BiomeBuilder().precipitation(Biome.Precipitation.NONE).biomeCategory(Biome.BiomeCategory.NONE).mobSpawnSettings(mobSpawnInfo).temperature(1.6f).downfall(0f).specialEffects(effects).generationSettings(biomeGenerationSettings.build()).build();
             event.getRegistry().register(orbit.setRegistryName(BossToolsMod.ModId,"orbit"));
         }
-    }
-
-    public static void addDefaultCarversAndLakesLava(BiomeGenerationSettings.Builder p_194721_) {
-        p_194721_.addCarver(GenerationStep.Carving.AIR, Carvers.CAVE);
-        p_194721_.addCarver(GenerationStep.Carving.AIR, Carvers.CAVE_EXTRA_UNDERGROUND);
-        p_194721_.addCarver(GenerationStep.Carving.AIR, Carvers.CANYON);
-        p_194721_.addFeature(GenerationStep.Decoration.LAKES, MiscOverworldPlacements.LAKE_LAVA_UNDERGROUND);
     }
 
     public static void addDefaultCarversAndLakes(BiomeGenerationSettings.Builder p_194721_) {
