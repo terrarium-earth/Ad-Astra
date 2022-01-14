6f4dcdf2c488160effbeea08e6a71d29e993ac21
gisellevonbingen <gisellevonbingen@gmail.com>
2022-01-14 21:14:48 +0900

    Update JeiPlugin.java

--- src/main/java/net/mrscauthd/beyond_earth/jei/JeiPlugin.java
+++ src/main/java/net/mrscauthd/beyond_earth/jei/JeiPlugin.java
@@ -37,7 +37,6 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Font;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.TextComponent;
 import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
