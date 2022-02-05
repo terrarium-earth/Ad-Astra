42e18d840f04b572ba284a1ac75f496ac4c43f37
MrScautHD <MrScautHD5@gmail.com>
2022-02-05 09:38:10 +0100

    Update AtlasEvent.java

--- src/main/java/net/mrscauthd/beyond_earth/events/AtlasEvent.java
+++ src/main/java/net/mrscauthd/beyond_earth/events/AtlasEvent.java
@@ -8,9 +8,9 @@ import net.minecraftforge.fml.common.Mod;
 import net.mrscauthd.beyond_earth.BeyondEarthMod;
 
 @Mod.EventBusSubscriber(modid = BeyondEarthMod.MODID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
-public class AtlassEvent {
+public class AtlasEvent {
     @SubscribeEvent
-    public static void AtlasEvent(TextureStitchEvent.Pre event) {
+    public static void addSprite(TextureStitchEvent.Pre event) {
         event.addSprite(new ResourceLocation(BeyondEarthMod.MODID, "entities/tile_entity_box_oxygen_generator"));
     }
 }
