1e246cbf2ac22fc7f03ab89a0793433e593596df
MrScautHD <MrScautHD5@gmail.com>
2022-03-20 10:04:21 +0100

    test 2

--- src/main/java/net/mrscauthd/beyond_earth/cable/EnergyCable.java
+++ src/main/java/net/mrscauthd/beyond_earth/cable/EnergyCable.java
@@ -3,195 +3,28 @@ package net.mrscauthd.beyond_earth.cable;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseEntityBlock;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.SimpleWaterloggedBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityTicker;
+import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraftforge.energy.CapabilityEnergy;
-import org.jetbrains.annotations.Nullable;
 
-import java.util.*;
-
-public class EnergyCable extends BaseEntityBlock implements SimpleWaterloggedBlock {
-
-    public static HashMap<BlockPos, List<BlockPos>> CABLENETWORKS = new HashMap<>();
-    public static HashMap<BlockPos,List<BlockPos>> CABLES = new HashMap<>();
+public class EnergyCable extends BaseEntityBlock {
 
     public EnergyCable(Properties p_49224_) {
         super(p_49224_);
     }
 
-    public void networkUpdate(BlockPos pos, Level worldIn){
-        if(!worldIn.isClientSide) {
-            //if(!CABLENETWORKS.containsKey(pos)){
-            //    CABLENETWORKS.put(pos,new ArrayList<BlockPos>());
-            //}
-            /*List<BlockPos> bl = new ArrayList<BlockPos>();
-            bl.add(pos.up());
-            bl.add(pos.down());
-            bl.add(pos.south());
-            bl.add(pos.north());
-            bl.add(pos.west());
-            bl.add(pos.east());*/
-
-            HashMap<BlockPos, List<BlockPos>> map = new HashMap<>();
-            HashMap<BlockPos, List<BlockPos>> map2 = new HashMap<>();
-
-            List<BlockPos> blocked = new ArrayList<BlockPos>();
-            blocked.add(pos);
-            this.generateNetwork(pos, map,map2, blocked, pos,  worldIn);
-            //for (BlockPos c : bl) {
-            //    if (worldIn.getBlockState(c).getBlock() instanceof EnergyCable) {
-            //        ((EnergyCable) worldIn.getBlockState(c).getBlock()).generateNetwork(pos, map, blocked, c, ((ServerWorld) worldIn));
-            //    }
-            //}
-
-            if (map.get(pos) == null) {
-                map.put(pos, new ArrayList<>());
-            }
-
-            boolean ALREADY_EXIST = true;
-
-            if (CABLENETWORKS.entrySet().size() == 0) {
-                System.out.println("System Added");
-                CABLENETWORKS.put(pos, map.get(pos));
-                CABLES.put(pos, map2.get(pos));
-            } else {
-                for (Map.Entry<BlockPos, List<BlockPos>> x : CABLENETWORKS.entrySet()) {
-                    ALREADY_EXIST = true;
-                    System.out.println("####TASK####");
-                    for (BlockPos p : map.get(pos)) {
-                        if (!(x.getValue().contains(p))) {
-                            ALREADY_EXIST = false;
-                            System.out.println("exist");
-                            //worldIn.setBlock(p, Blocks.DIAMOND_BLOCK.defaultBlockState(), 2);
-                        } else {
-                            System.out.println("don't exist");
-                        }
-                    }
-                    if (ALREADY_EXIST) {
-                        break;
-                    }
-                }
-
-                if ((!ALREADY_EXIST) && (map.get(pos).size() > 0)) {
-                    System.out.println("System Added");
-                    CABLENETWORKS.put(pos, map.get(pos));
-                    CABLES.put(pos, map2.get(pos));
-                } else {
-                    System.out.println("already exist");
-                }
-            }
-        }
-    }
-
-    @Override
-    public void onPlace(BlockState p_60566_, Level p_60567_, BlockPos p_60568_, BlockState p_60569_, boolean p_60570_) {
-        super.onPlace(p_60566_, p_60567_, p_60568_, p_60569_, p_60570_);
-        System.out.println("x");
-        System.out.println("placed");
-        this.networkUpdate(p_60568_, p_60567_);
-    }
-
-    public void generateNetwork(BlockPos engine, HashMap<BlockPos, List<BlockPos>> machines, HashMap<BlockPos,List<BlockPos>> cable, List<BlockPos> blocked, BlockPos checkedblock, Level world){
-        blocked.add(checkedblock);
-        /*if(!INPUTS.containsKey(checkedblock)){
-            INPUTS.put(checkedblock,new ArrayList<BlockPos>());
-        }
-        List<BlockPos> bp = INPUTS.get(checkedblock);
-        bp.add(engine);
-        INPUTS.put(checkedblock,bp);*/
-
-        if(machines.get(engine) == null){
-            machines.put(engine,new ArrayList<>());
-        }
-        if(cable.get(engine) == null){
-            cable.put(engine,new ArrayList<>());
-        }
-
-        if(world.getBlockState(checkedblock).getBlock() instanceof EnergyCable){
-            List<BlockPos> ca = cable.get(engine);
-            ca.add(checkedblock);
-            cable.put(engine,ca);
-        }
-
-        List<BlockPos> bl = new ArrayList<BlockPos>();
-        bl.add(checkedblock.above());
-        bl.add(checkedblock.below());
-        bl.add(checkedblock.south());
-        bl.add(checkedblock.north());
-        bl.add(checkedblock.west());
-        bl.add(checkedblock.east());
-
-        for (BlockPos c : bl) {
-            if(!blocked.contains(c)) {
-                if (world.getBlockState(c).getBlock() instanceof EnergyCable) {
-                    ((EnergyCable) world.getBlockState(c).getBlock()).generateNetwork(engine, machines,cable, blocked, c, world);
-                }else {
-                    if(this.MachineCanRecieve(world, c)||this.MachineCanExtract(world,c)) {
-                        System.out.println("machine"+engine.toString()+" find at: "+c.toString());
-                        blocked.add(c);
-                        List<BlockPos> pos = machines.get(engine);
-                        pos.add(c);
-                        machines.put(engine, pos);
-                    }
-
-                }
-            }
-        }
-
-    }
-
-    @Override
-    public void neighborChanged(BlockState state, Level worldIn, BlockPos pos, Block blockIn, BlockPos fromPos, boolean isMoving) {
-        super.neighborChanged(state, worldIn, pos, blockIn, fromPos, isMoving);
-
-
-        if(!worldIn.isClientSide){
-            //if(!CABLENETWORKS.containsKey(pos)){
-            //    CABLENETWORKS.put(pos,new ArrayList<BlockPos>());
-            //}
-
-            //List<BlockPos> BLOCKS = CABLENETWORKS.get(pos);
-            //INPUTS.clear();
-            //for (BlockPos bp : BLOCKS) {
-            //System.out.println("update");
-            //if (worldIn.getBlockState(bp).getBlock() instanceof MachineBlock) {
-            //((MachineBlock)worldIn.getBlockState(bp).getBlock()).INPUTS.remove(bp);
-            try {
-                for (Map.Entry<BlockPos,List<BlockPos>>x :CABLENETWORKS.entrySet()) {
-                    if(x.getValue().contains(pos)){
-                        CABLENETWORKS.remove(x.getKey());
-                        CABLES.remove(x.getKey());
-                    }
-                }
-            } catch (ConcurrentModificationException cme){
-
-            }
-
-            this.networkUpdate(pos,worldIn);
-            //((MachineBlock)worldIn.getBlockState(bp).getBlock()).networkUpdate(pos,((World)worldIn));
-        }
-    }
-
-
-    public boolean MachineCanRecieve(Level world,BlockPos pos){
-        return Optional.ofNullable(world.getBlockEntity(pos))
-                .flatMap(te -> te.getCapability(CapabilityEnergy.ENERGY).resolve())
-                .map(cap -> cap.canReceive())
-                .orElse(false);
-    }
-
-    public boolean MachineCanExtract(Level world, BlockPos pos){
-        return Optional.ofNullable(world.getBlockEntity(pos))
-                .flatMap(te -> te.getCapability(CapabilityEnergy.ENERGY).resolve())
-                .map(cap -> cap.canExtract())
-                .orElse(false);
-    }
-
-    @Nullable
     @Override
     public BlockEntity newBlockEntity(BlockPos p_153215_, BlockState p_153216_) {
         return new EnergyCableTileEntity(p_153215_, p_153216_);
+    }
+
+    @Override
+    public <T2 extends BlockEntity> BlockEntityTicker<T2> getTicker(Level level, BlockState state, BlockEntityType<T2> type) {
+        return (l, p, s, e) -> {
+            if (e instanceof EnergyCableTileEntity) {
+                ((EnergyCableTileEntity) e).tick();
+            }
+        };
     }
 }
--- src/main/java/net/mrscauthd/beyond_earth/cable/EnergyCableTileEntity.java
+++ src/main/java/net/mrscauthd/beyond_earth/cable/EnergyCableTileEntity.java
@@ -1,187 +1,73 @@
 package net.mrscauthd.beyond_earth.cable;
 
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraftforge.common.capabilities.Capability;
-import net.minecraftforge.common.util.LazyOptional;
-import net.minecraftforge.energy.CapabilityEnergy;
-import net.minecraftforge.energy.EnergyStorage;
-import net.minecraftforge.energy.IEnergyStorage;
-import net.minecraftforge.fluids.capability.CapabilityFluidHandler;
-import net.minecraftforge.items.CapabilityItemHandler;
 import net.mrscauthd.beyond_earth.ModInit;
-import net.mrscauthd.beyond_earth.machines.tile.PowerSystem;
 
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
-public class EnergyCableTileEntity extends BlockEntity implements BlockEntityTicker {
-
-    //protected List<Block> network = new ArrayList<>();
-    public static final int ENERGY_PER_TICK = 1;
+public class EnergyCableTileEntity extends BlockEntity {
 
     public EnergyCableTileEntity(BlockPos pos, BlockState state) {
         super(ModInit.ENERGY_CABLE.get(), pos, state);
     }
 
-    @Override
-    public void tick(Level p_155253_, BlockPos p_155254_, BlockState p_155255_, BlockEntity p_155256_) {
-        if(!this.level.isClientSide) {
-            if (this.level.getBlockState(this.getBlockPos()).getBlock() instanceof EnergyCable) {
-                EnergyCable ec = (EnergyCable) this.level.getBlockState(this.getBlockPos()).getBlock();
-                if (EnergyCable.CABLENETWORKS.get(this.getBlockPos()) != null) {
-                    List<BlockPos> RECIEVE = new ArrayList<>();
-                    List<BlockPos> EXTRACT = new ArrayList<>();
-
-                    for (BlockPos p:EnergyCable.CABLENETWORKS.get(getBlockPos())) {
-                        if(this.canExtract(level,p)){
-                            EXTRACT.add(p);
-                        }else if(this.canRecieve(level,p)){
-                            RECIEVE.add(p);
-                        }
-                    }
-
-                    for (BlockPos c : RECIEVE) {
-                        for(BlockPos e : EnergyCable.CABLES.get(getBlockPos())){
-                            BlockEntity et = this.level.getBlockEntity(e);
-                            BlockEntity ct = this.level.getBlockEntity(c);
-
-                            if(et == null||ct==null){
-                                continue;
-                            }
-
-                            if(this.getEnergy(level,e) >= 1&&this.getEnergy(level,c) < this.getMaxEnergy(level,c)){
-
-                            level.setBlock(c, Blocks.BEDROCK.defaultBlockState(), 2);
-                            System.out.println("work");
-                            et.getCapability(CapabilityEnergy.ENERGY, Direction.NORTH).ifPresent(capability -> capability.extractEnergy(1, true));
-                            ct.getCapability(CapabilityEnergy.ENERGY, Direction.NORTH).ifPresent(capability -> capability.receiveEnergy(1, true));
-                            }
-                        }
-                    }
-
-                    //System.out.println("handle");
-                    /*for (BlockPos pb : EnergyCable.CABLENETWORKS.get(pos)) {
-                        TileEntity fr = this.world.getTileEntity(pb);
-                        if (fr == null) {
-                            System.out.println("contiune");
-                            continue;
-                        }
-                        for (BlockPos bp : EnergyCable.CABLENETWORKS.get(pos)) {
-                            if (bp.getCoordinatesAsString().equals(pb.getCoordinatesAsString())) {
-                                continue;
-                            }
-                            TileEntity te = this.world.getTileEntity(bp);
-                            if (te == null) {
-                                System.out.println("contiune");
-                                continue;
-                            }
-                            if (this.canRecieve(world, pb) || this.canExtract(world, bp)) {
-                                if (this.getEnergy(world, bp) > 0 && this.getEnergy(world, pb) < this.getMaxEnergy(world, pb)) {
-                                    te.getCapability(CapabilityEnergy.ENERGY, Direction.NORTH).ifPresent(capability -> capability.extractEnergy(1, false));
-                                    fr.getCapability(CapabilityEnergy.ENERGY, Direction.NORTH).ifPresent(capability -> capability.receiveEnergy(1, false));
-                                    System.out.println("send");
-                                    world.getPlayers().get(0).sendMessage(ITextComponent.getTextComponentOrEmpty("send"),null);
-                                }else {
-                                    world.getPlayers().get(0).sendMessage(ITextComponent.getTextComponentOrEmpty("its worng"),null);
-                                }
-                            }
-                        }
-                    }*/
-                }
-            }
-        }
+    public void tick() {
+        /*for (BlockPos f1 : this.isMachine(level, this.getBlockPos())) {
+            level.setBlock(f1, Blocks.BEDROCK.defaultBlockState(), 2);
+            System.out.println(f1);
+        }*/
     }
 
-    public EnergyStorage ENERGY = new EnergyStorage(9000, 200, 200, 0) {
-        @Override
-        public int receiveEnergy(int maxReceive, boolean simulate) {
-            int retval = super.receiveEnergy(maxReceive, simulate);
-            if (!simulate) {
-                setChanged();
-                level.sendBlockUpdated(getBlockPos(), level.getBlockState(getBlockPos()), level.getBlockState(getBlockPos()), 2);
-            }
-            return retval;
-        }
+    public List<BlockPos> directionList(BlockPos pos) {
+        List<BlockPos> dir = new ArrayList<>();
 
-        @Override
-        public int extractEnergy(int maxExtract, boolean simulate) {
-            int retval = super.extractEnergy(maxExtract, simulate);
-            if (!simulate) {
-                setChanged();
-                level.sendBlockUpdated(getBlockPos(), level.getBlockState(getBlockPos()), level.getBlockState(getBlockPos()), 2);
-            }
-            return retval;
-        }
+        dir.add(pos.above());
+        dir.add(pos.below());
+        dir.add(pos.north());
+        dir.add(pos.south());
+        dir.add(pos.east());
+        dir.add(pos.west());
 
-        @Override
-        public boolean canExtract() {
-            return true;
-        }
-
-        @Override
-        public int getMaxEnergyStored() {
-            return 1000;
-        }
-    };
-
-    public boolean canRecieve(Level world, BlockPos pos){
-        return Optional.ofNullable(world.getBlockEntity(pos))
-                .flatMap(te -> te.getCapability(CapabilityEnergy.ENERGY).resolve())
-                .map(cap -> cap.canReceive())
-                .orElse(false);
+        return dir;
     }
 
-    public boolean canExtract(Level world, BlockPos pos){
-        return Optional.ofNullable(world.getBlockEntity(pos))
-                .flatMap(te -> te.getCapability(CapabilityEnergy.ENERGY).resolve())
-                .map(cap -> cap.canExtract())
-                .orElse(false);
-    }
+    public List<BlockPos> isMachine(Level level, BlockPos pos) {
+        /** Machine List */
+        List<BlockPos> list = new ArrayList<>();
 
-    public int getEnergy(Level world,BlockPos pos){
-        return Optional.ofNullable(world.getBlockEntity(pos))
-                .flatMap(te -> te.getCapability(CapabilityEnergy.ENERGY).resolve())
-                .map(cap -> cap.getEnergyStored())
-                .orElse(0);
-    }
-
-    public int getMaxEnergy(Level world,BlockPos pos){
-        return Optional.ofNullable(world.getBlockEntity(pos))
-                .flatMap(te -> te.getCapability(CapabilityEnergy.ENERGY).resolve())
-                .map(cap -> cap.getMaxEnergyStored())
-                .orElse(0);
-    }
-
-    @Override
-    public <T> LazyOptional<T> getCapability(Capability<T> capability, @Nullable Direction facing) {
-        if (!this.isRemoved()) {
-             if (capability == CapabilityEnergy.ENERGY) {
-                LazyOptional<T> optional = this.getCapabilityEnergy(capability, facing);
-                if (optional != null && optional.isPresent()) {
-                    return optional;
-                }
+        for (BlockPos f1 : this.directionList(pos)) {
+            if (level.getBlockState(f1).is(Blocks.DIAMOND_BLOCK)) {
+                list.add(f1);
             }
         }
 
-        return super.getCapability(capability, facing);
+        return list;
     }
 
-    public <T> LazyOptional<T> getCapabilityEnergy(Capability<T> capability, @Nullable Direction facing) {
-        IEnergyStorage energyStorage = ENERGY;
+    public List<BlockPos> isCable(Level level, BlockPos pos) {
 
-        if (energyStorage != null) {
-            return LazyOptional.of(() -> energyStorage).cast();
+        /** Naigtbor List */
+        List<BlockPos> network = new ArrayList<>();
+
+        for (BlockPos f1 : this.directionList(pos)) {
+            if (level.getBlockState(f1).is(ModInit.ENERGY_CABLE_BLOCK.get())) {
+                network.add(f1);
+            }
         }
 
-        return LazyOptional.empty();
+        /** loop */
+        for (BlockPos f2 : network) {
+            for (BlockPos f3 : directionList(f2)) {
+
+            }
+        }
+
+        return null;
     }
 }
