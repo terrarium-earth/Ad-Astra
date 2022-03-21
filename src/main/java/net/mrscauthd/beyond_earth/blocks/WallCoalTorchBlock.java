55d23ef1595e2384287e38e9f87cb6013252dc98
Alex Nijjar <61668546+AlexNijjar@users.noreply.github.com>
2022-03-20 20:35:08 -0600

    Initial Port Changes
    - All of the basic blocks and items have been added.
    - Working flags that display the player's head.
    - Fully-rendered 3D space suit and netherite space suit.
    - Hammer crafting with custom serialization.
    - Oil and Fuel fluids.
    - Working coal torches and lanterns.
    
    Signed-off-by: Alex Nijjar <alexander.nijjar@icloud.com>

---
+++ src/main/java/net/mrscauthd/beyond_earth/blocks/WallCoalTorchBlock.java
@@ -0,0 +1,95 @@
+package net.mrscauthd.beyond_earth.blocks;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Maps;
+import net.minecraft.block.*;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.DirectionProperty;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.BlockMirror;
+import net.minecraft.util.BlockRotation;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
+import net.minecraft.world.WorldAccess;
+import net.minecraft.world.WorldView;
+import net.mrscauthd.beyond_earth.BeyondEarth;
+
+import java.util.Map;
+
+public class WallCoalTorchBlock extends CoalTorchBlock {
+
+    public static final DirectionProperty FACING = HorizontalFacingBlock.FACING;
+    private static final Map<Direction, VoxelShape> BOUNDING_SHAPES = Maps.newEnumMap(ImmutableMap.of(Direction.NORTH, Block.createCuboidShape(5.5D, 3.0D, 11.0D, 10.5D, 13.0D, 16.0D), Direction.SOUTH, Block.createCuboidShape(5.5D, 3.0D, 0.0D, 10.5D, 13.0D, 5.0D), Direction.WEST, Block.createCuboidShape(11.0D, 3.0D, 5.5D, 16.0D, 13.0D, 10.5D), Direction.EAST, Block.createCuboidShape(0.0D, 3.0D, 5.5D, 5.0D, 13.0D, 10.5D)));
+
+    public WallCoalTorchBlock(Settings settings) {
+        super(settings);
+        this.setDefaultState(this.stateManager.getDefaultState().with(FACING, Direction.NORTH));
+    }
+
+    @Override
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+        return super.onUse(state, world, pos, player, hand, hit);
+    }
+
+    @Override
+    public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        return getBoundingShape(state);
+    }
+
+    public static VoxelShape getBoundingShape(BlockState state) {
+        return BOUNDING_SHAPES.get(state.get(FACING));
+    }
+
+    @Override
+    public boolean canPlaceAt(BlockState state, WorldView world, BlockPos pos) {
+        Direction direction = state.get(FACING);
+        BlockPos blockPos = pos.offset(direction.getOpposite());
+        BlockState blockState = world.getBlockState(blockPos);
+        return blockState.isSideSolidFullSquare(world, blockPos, direction);
+    }
+
+    @Override
+    public BlockState getPlacementState(ItemPlacementContext ctx) {
+        BlockState blockState = this.getDefaultState();
+        WorldView worldView = ctx.getWorld();
+        BlockPos blockPos = ctx.getBlockPos();
+
+        for (Direction direction : ctx.getPlacementDirections()) {
+            if (direction.getAxis().isHorizontal()) {
+                blockState = blockState.with(FACING, direction.getOpposite());
+                if (blockState.canPlaceAt(worldView, blockPos)) {
+                    return blockState;
+                }
+            }
+        }
+
+        return null;
+    }
+
+    @Override
+    public BlockState getStateForNeighborUpdate(BlockState state, Direction direction, BlockState neighborState, WorldAccess world, BlockPos pos, BlockPos neighborPos) {
+        return direction.getOpposite() == state.get(FACING) && !state.canPlaceAt(world, pos) ? Blocks.AIR.getDefaultState() : state;
+    }
+
+    @Override
+    public BlockState rotate(BlockState state, BlockRotation rotation) {
+        return state.with(FACING, rotation.rotate(state.get(FACING)));
+    }
+
+    @Override
+    public BlockState mirror(BlockState state, BlockMirror mirror) {
+        return state.rotate(mirror.getRotation(state.get(FACING)));
+    }
+
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
+    }
+}
