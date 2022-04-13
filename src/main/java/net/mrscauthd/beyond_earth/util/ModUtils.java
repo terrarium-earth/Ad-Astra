0e7bacee62027acdf05ba8e468d09b23da529fb2
Alex Nijjar <61668546+AlexNijjar@users.noreply.github.com>
2022-04-13 02:37:53 -0600

    New Teleport system and mars
    - Added mars sky.
    - New teleportation system when falling out of orbit.

--- src/main/java/net/mrscauthd/beyond_earth/util/ModUtils.java
+++ src/main/java/net/mrscauthd/beyond_earth/util/ModUtils.java
@@ -1,6 +1,10 @@
 package net.mrscauthd.beyond_earth.util;
 
-import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.ItemEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.util.registry.RegistryKey;
@@ -13,6 +17,7 @@ import java.util.Objects;
 
 public class ModUtils {
 
+    public static final int SPAWN_START = 450;
     public static final RegistryKey<World> EARTH_ORBIT_KEY = RegistryKey.of(Registry.WORLD_KEY, new ModIdentifier("earth_orbit"));
     public static final RegistryKey<World> MOON_KEY = RegistryKey.of(Registry.WORLD_KEY, new ModIdentifier("moon"));
     public static final RegistryKey<World> MOON_ORBIT_KEY = RegistryKey.of(Registry.WORLD_KEY, new ModIdentifier("moon_orbit"));
@@ -38,16 +43,56 @@ public class ModUtils {
             GLACIO_ORBIT_KEY
     );
 
+    private static final List<RegistryKey<World>> planets = Arrays.asList(
+            World.OVERWORLD,
+            MOON_KEY,
+            MARS_KEY,
+            VENUS_KEY,
+            MERCURY_KEY,
+            GLACIO_KEY
+    );
 
-    public static void teleportToPlanet(RegistryKey<World> dimension, ServerPlayerEntity player) {
-        if (player.canUsePortals()) {
-            ServerWorld targetDimension = Objects.requireNonNull(player.getServer()).getWorld(getCorrespondingOrbitDimension(dimension));
-            player.teleport(targetDimension, player.getX(), 450.0, player.getZ(), player.getYaw(), player.getPitch());
+
+    public static void teleportToPlanet(RegistryKey<World> dimension, Entity entity) {
+        if (!entity.getWorld().isClient && entity.canUsePortals()) {
+            MinecraftServer server = entity.getServer();
+            ServerWorld targetDimension = Objects.requireNonNull(server).getWorld(getCorrespondingOrbitDimension(dimension));
+
+            if (targetDimension == null) {
+                BeyondEarth.LOGGER.info("Invalid target dimension!");
+                return;
+            }
+
+            entity = entity.moveToWorld(targetDimension);
+
+            // Cook food.
+            if (entity instanceof ItemEntity item) {
+                ItemStack stack = item.getStack();
+                if (stack.getItem().equals(Items.CHICKEN)) {
+                    item.setStack(new ItemStack(Items.COOKED_CHICKEN, stack.getCount()));
+                } else if (stack.getItem().equals(Items.BEEF)) {
+                    item.setStack(new ItemStack(Items.COOKED_BEEF, stack.getCount()));
+                } else if (stack.getItem().equals(Items.PORKCHOP)) {
+                    item.setStack(new ItemStack(Items.COOKED_PORKCHOP, stack.getCount()));
+                } else if (stack.getItem().equals(Items.MUTTON)) {
+                    item.setStack(new ItemStack(Items.COOKED_MUTTON, stack.getCount()));
+                } else if (stack.getItem().equals(Items.RABBIT)) {
+                    item.setStack(new ItemStack(Items.COOKED_RABBIT, stack.getCount()));
+                } else if (stack.getItem().equals(Items.COD)) {
+                    item.setStack(new ItemStack(Items.COOKED_COD, stack.getCount()));
+                } else if (stack.getItem().equals(Items.SALMON)) {
+                    item.setStack(new ItemStack(Items.COOKED_SALMON, stack.getCount()));
+                }
+            }
         }
     }
 
     public static boolean isOrbitDimension(RegistryKey<World> dimension) {
         return dimension.equals(EARTH_ORBIT_KEY) || dimension.equals(MOON_ORBIT_KEY) || dimension.equals(MARS_ORBIT_KEY) || dimension.equals(VENUS_ORBIT_KEY) || dimension.equals(MERCURY_ORBIT_KEY) || dimension.equals(GLACIO_ORBIT_KEY);
+    }
+
+    public static boolean isPlanet(RegistryKey<World> dimension) {
+        return planets.stream().anyMatch(dimension::equals);
     }
 
     public static RegistryKey<World> getCorrespondingOrbitDimension(RegistryKey<World> dimension) {
@@ -64,6 +109,7 @@ public class ModUtils {
         } else if (dimension.equals(GLACIO_ORBIT_KEY)) {
             return GLACIO_KEY;
         } else {
+            BeyondEarth.LOGGER.info("Not an orbit dimension!");
             return World.OVERWORLD;
         }
     }
