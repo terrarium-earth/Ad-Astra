4c67d2f6c148c7c705de1d6877532fd7e4414243
AlexNijjar <61668546+AlexNijjar@users.noreply.github.com>
2022-08-10 22:00:33 -0500

    Finished EMI Support

--- src/main/java/com/github/alexnijjar/beyond_earth/compat/emi/EmiFluidBarWidget.java
+++ src/main/java/com/github/alexnijjar/beyond_earth/compat/emi/EmiFluidBarWidget.java
@@ -7,7 +7,6 @@ import dev.emi.emi.api.widget.Bounds;
 import dev.emi.emi.api.widget.Widget;
 import net.fabricmc.fabric.api.transfer.v1.client.fluid.FluidVariantRendering;
 import net.fabricmc.fabric.api.transfer.v1.fluid.FluidVariant;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawableHelper;
 import net.minecraft.client.texture.Sprite;
 import net.minecraft.client.util.math.MatrixStack;
@@ -20,19 +19,19 @@ public class EmiFluidBarWidget extends Widget {
     private int speed;
     private boolean increase;
 
-	public EmiFluidBarWidget(int x, int y,  FluidVariant fluid, int speed, boolean increase) {
-		this.x = x;
+    public EmiFluidBarWidget(int x, int y, FluidVariant fluid, int speed, boolean increase) {
+        this.x = x;
         this.y = y;
         this.fluid = fluid;
         this.speed = speed;
         this.increase = increase;
-        
-	}
 
-	@Override
-	public Bounds getBounds() {
-		return new Bounds(0, 0, 0, 0);
-	}
+    }
+
+    @Override
+    public Bounds getBounds() {
+        return new Bounds(0, 0, 0, 0);
+    }
 
     @Override
     public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
@@ -41,20 +40,23 @@ public class EmiFluidBarWidget extends Widget {
         int tankY = y + 3;
 
         double ratio = (GuiUtil.FLUID_TANK_HEIGHT - MathHelper.ceil((System.currentTimeMillis() / (speed / GuiUtil.FLUID_TANK_HEIGHT) % GuiUtil.FLUID_TANK_HEIGHT))) / (double) GuiUtil.FLUID_TANK_HEIGHT;
-        
+
         if (increase) {
             drawFluid(matrices, tankX, tankY, 1.0 - ratio, fluid);
         } else {
             drawFluid(matrices, tankX, tankY, ratio, fluid);
         }
 
+        // Draw the lines texture.
         RenderSystem.enableBlend();
         GuiUtil.drawVertical(new MatrixStack(), tankX, tankY, GuiUtil.FLUID_TANK_WIDTH, GuiUtil.FLUID_TANK_HEIGHT, GuiUtil.FLUID_TANK_TEXTURE, 1.0);
         RenderSystem.disableBlend();
     }
-    
-    private static void drawFluid(MatrixStack matrices, int x, int y, double ratio, FluidVariant fluid) {
 
+    // Draws fluid in a bit of a weird way. Normally in all the other GUIs, the fluid is drawn in full, and then cut with
+    // GL_SCISSOR. However, this is seems to be broken in the EMI gui, so instead, the sprite is scaled, causing the texture to look
+    // pinched.
+    private static void drawFluid(MatrixStack matrices, int x, int y, double ratio, FluidVariant fluid) {
         if (fluid.isBlank()) {
             return;
         }
@@ -66,23 +68,25 @@ public class EmiFluidBarWidget extends Widget {
         RenderSystem.setShaderColor((colour >> 16 & 255) / 255.0f, (float) (colour >> 8 & 255) / 255.0f, (float) (colour & 255) / 255.0f, 1.0f);
         RenderSystem.setShaderTexture(0, PlayerScreenHandler.BLOCK_ATLAS_TEXTURE);
 
-        MinecraftClient client = MinecraftClient.getInstance();
-        double scale = (int) client.getWindow().getScaleFactor();
-        int screenHeight = client.getWindow().getScaledHeight();
+        for (int i = 0; i < 3; i++) {
 
-        int scissorX = (int) (x * scale);
-        int scissorY = (int) (((screenHeight - y - GuiUtil.FLUID_TANK_HEIGHT)) * scale);
-        int scissorWidth = (int) (GuiUtil.FLUID_TANK_WIDTH * scale);
-        int scissorHeight = (int) ((GuiUtil.FLUID_TANK_HEIGHT - 1) * scale * ratio);
+            int height = spriteHeight;
 
-        RenderSystem.enableScissor(scissorX, scissorY, scissorWidth, scissorHeight);
+            if (i == 0 && ratio < 0.33) {
+                height = (int) (spriteHeight * ratio * 3);
+            }
+            if (i == 1 && ratio < 0.66) {
+                height = (int) (spriteHeight * ratio * 3) - spriteHeight;
+            }
+            if (i == 2) {
+                height = (int) (spriteHeight * ratio * 3) - spriteHeight - spriteHeight;
+            }
 
-        for (int i = 1; i < 4; i++) {
-            DrawableHelper.drawSprite(matrices, x + 1, GuiUtil.FLUID_TANK_HEIGHT + y - (spriteHeight * i), 0, GuiUtil.FLUID_TANK_WIDTH - 2, spriteHeight, sprite);
-            // DrawableHelper.drawSprite(matrices, x - 400 + 1, GuiUtil.FLUID_TANK_HEIGHT + y - 200 - (spriteHeight * i), 0, GuiUtil.FLUID_TANK_WIDTH * 40 - 2, spriteHeight * 40, sprite);
+            int yOffset = y + spriteHeight - height;
+
+            DrawableHelper.drawSprite(matrices, x + 1, GuiUtil.FLUID_TANK_HEIGHT + yOffset - (spriteHeight * (i + 1)), 0, GuiUtil.FLUID_TANK_WIDTH - 2, height, sprite);
         }
 
-        RenderSystem.disableScissor();
         RenderSystem.setShaderColor(1.0f, 1.0f, 1.0f, 1.0f);
     }
 }
