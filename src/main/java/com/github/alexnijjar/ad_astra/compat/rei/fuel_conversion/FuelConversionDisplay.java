db96c79f73fac0e2ef6f46cf63d479930668461f
AlexNijjar <61668546+AlexNijjar@users.noreply.github.com>
2022-09-02 23:04:42 -0500

    Re-added REI support

--- src/main/java/com/github/alexnijjar/ad_astra/compat/rei/fuel_conversion/FuelConversionDisplay.java
+++ src/main/java/com/github/alexnijjar/ad_astra/compat/rei/fuel_conversion/FuelConversionDisplay.java
@@ -1,31 +1,31 @@
-// package com.github.alexnijjar.ad_astra.compat.rei.fuel_conversion;
+package com.github.alexnijjar.ad_astra.compat.rei.fuel_conversion;
 
-// import com.github.alexnijjar.ad_astra.compat.rei.REICategories;
-// import com.github.alexnijjar.ad_astra.recipes.FuelConversionRecipe;
-// import me.shedaniel.rei.api.common.category.CategoryIdentifier;
-// import me.shedaniel.rei.api.common.display.Display;
-// import me.shedaniel.rei.api.common.entry.EntryIngredient;
-// import me.shedaniel.rei.api.common.util.EntryIngredients;
-// import net.fabricmc.api.EnvType;
-// import net.fabricmc.api.Environment;
+import com.github.alexnijjar.ad_astra.compat.rei.REICategories;
+import com.github.alexnijjar.ad_astra.recipes.FuelConversionRecipe;
+import me.shedaniel.rei.api.common.category.CategoryIdentifier;
+import me.shedaniel.rei.api.common.display.Display;
+import me.shedaniel.rei.api.common.entry.EntryIngredient;
+import me.shedaniel.rei.api.common.util.EntryIngredients;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 
-// import java.util.List;
+import java.util.List;
 
-// @Environment(EnvType.CLIENT)
-// public record FuelConversionDisplay(FuelConversionRecipe recipe) implements Display {
+@Environment(EnvType.CLIENT)
+public record FuelConversionDisplay(FuelConversionRecipe recipe) implements Display {
 
-// 	@Override
-// 	public List<EntryIngredient> getInputEntries() {
-// 		return List.of(EntryIngredients.of(recipe.getFluidInput()));
-// 	}
+	@Override
+	public List<EntryIngredient> getInputEntries() {
+		return List.of(EntryIngredients.of(recipe.getFluidInput()));
+	}
 
-// 	@Override
-// 	public List<EntryIngredient> getOutputEntries() {
-// 		return List.of(EntryIngredients.of(recipe.getFluidOutput()));
-// 	}
+	@Override
+	public List<EntryIngredient> getOutputEntries() {
+		return List.of(EntryIngredients.of(recipe.getFluidOutput()));
+	}
 
-// 	@Override
-// 	public CategoryIdentifier<?> getCategoryIdentifier() {
-// 		return REICategories.FUEL_CONVERSION_CATEGORY;
-// 	}
-// }
+	@Override
+	public CategoryIdentifier<?> getCategoryIdentifier() {
+		return REICategories.FUEL_CONVERSION_CATEGORY;
+	}
+}
--- src/main/java/com/github/alexnijjar/ad_astra/compat/rei/oxygen_conversion/OxygenConversionDisplay.java
+++ src/main/java/com/github/alexnijjar/ad_astra/compat/rei/oxygen_conversion/OxygenConversionDisplay.java
@@ -1,31 +1,31 @@
-// package com.github.alexnijjar.ad_astra.compat.rei.oxygen_conversion;
+package com.github.alexnijjar.ad_astra.compat.rei.oxygen_conversion;
 
-// import com.github.alexnijjar.ad_astra.compat.rei.REICategories;
-// import com.github.alexnijjar.ad_astra.recipes.OxygenConversionRecipe;
-// import me.shedaniel.rei.api.common.category.CategoryIdentifier;
-// import me.shedaniel.rei.api.common.display.Display;
-// import me.shedaniel.rei.api.common.entry.EntryIngredient;
-// import me.shedaniel.rei.api.common.util.EntryIngredients;
-// import net.fabricmc.api.EnvType;
-// import net.fabricmc.api.Environment;
+import com.github.alexnijjar.ad_astra.compat.rei.REICategories;
+import com.github.alexnijjar.ad_astra.recipes.OxygenConversionRecipe;
+import me.shedaniel.rei.api.common.category.CategoryIdentifier;
+import me.shedaniel.rei.api.common.display.Display;
+import me.shedaniel.rei.api.common.entry.EntryIngredient;
+import me.shedaniel.rei.api.common.util.EntryIngredients;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 
-// import java.util.List;
+import java.util.List;
 
-// @Environment(EnvType.CLIENT)
-// public record OxygenConversionDisplay(OxygenConversionRecipe recipe) implements Display {
+@Environment(EnvType.CLIENT)
+public record OxygenConversionDisplay(OxygenConversionRecipe recipe) implements Display {
 
-// 	@Override
-// 	public List<EntryIngredient> getInputEntries() {
-// 		return List.of(EntryIngredients.of(recipe.getFluidInput()));
-// 	}
+	@Override
+	public List<EntryIngredient> getInputEntries() {
+		return List.of(EntryIngredients.of(recipe.getFluidInput()));
+	}
 
-// 	@Override
-// 	public List<EntryIngredient> getOutputEntries() {
-// 		return List.of(EntryIngredients.of(recipe.getFluidOutput()));
-// 	}
+	@Override
+	public List<EntryIngredient> getOutputEntries() {
+		return List.of(EntryIngredients.of(recipe.getFluidOutput()));
+	}
 
-// 	@Override
-// 	public CategoryIdentifier<?> getCategoryIdentifier() {
-// 		return REICategories.OXYGEN_CONVERSION_CATEGORY;
-// 	}
-// }
+	@Override
+	public CategoryIdentifier<?> getCategoryIdentifier() {
+		return REICategories.OXYGEN_CONVERSION_CATEGORY;
+	}
+}
