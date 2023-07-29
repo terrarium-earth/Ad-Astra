2901aca34052e0dc3df8aed228cf8e58d4a03f4c
Alex Nijjar <alexander.nijjar@icloud.com>
2023-07-29 15:58:17 -0500

    Fix water bucket issue on forge

--- forge/src/main/java/earth/terrarium/ad_astra/mixin/forge/BucketItemMixin.java
+++ forge/src/main/java/earth/terrarium/ad_astra/mixin/forge/BucketItemMixin.java
@@ -1,4 +1,4 @@
-package earth.terrarium.ad_astra.mixin;
+package earth.terrarium.ad_astra.mixin.forge;
 
 import earth.terrarium.ad_astra.common.util.ModUtils;
 import earth.terrarium.ad_astra.common.util.OxygenUtils;
@@ -7,19 +7,14 @@ import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.tags.FluidTags;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.BucketItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.gameevent.GameEvent;
 import net.minecraft.world.level.material.Fluid;
-import net.minecraft.world.level.material.Fluids;
 import net.minecraft.world.phys.BlockHitResult;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Final;
@@ -40,25 +35,13 @@ public abstract class BucketItemMixin extends Item {
         super(properties);
     }
 
-    @Inject(method = "use", at = @At("HEAD"), cancellable = true)
-    private void ad_astra$use(Level level, Player player, InteractionHand usedHand, CallbackInfoReturnable<InteractionResultHolder<ItemStack>> cir) {
-        if (!(ModUtils.getWorldTemperature(level) < 0)) return;
-        BlockHitResult blockhitresult = getPlayerPOVHitResult(level, player, this.content == Fluids.EMPTY ? ClipContext.Fluid.SOURCE_ONLY : ClipContext.Fluid.NONE);
-        BlockPos pos = blockhitresult.getBlockPos();
-        if (OxygenUtils.posHasOxygen(level, pos) || OxygenUtils.posHasOxygen(level, pos.above())) return;
-        BlockState blockState = level.getBlockState(pos);
-        if (!blockState.hasProperty(BlockStateProperties.WATERLOGGED)) return;
-        cir.setReturnValue(InteractionResultHolder.fail(player.getItemInHand(usedHand)));
-    }
-
-    // Allow buckets to be used in hot dimensions when there is oxygen.
     @Inject(
-        method = "emptyContents",
+        method = "emptyContents(Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/phys/BlockHitResult;Lnet/minecraft/world/item/ItemStack;)Z",
         at = @At(
             value = "HEAD",
             target = "Lnet/minecraft/world/item/BucketItem;emptyContents(Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/phys/BlockHitResult;)Z"),
         cancellable = true)
-    private void ad_astra$emptyContents(@Nullable Player player, Level level, BlockPos pos, @Nullable BlockHitResult blockHitResult, CallbackInfoReturnable<Boolean> cir) {
+    private void ad_astra$emptyContents(@Nullable Player player, Level level, BlockPos pos, @Nullable BlockHitResult blockHitResult, ItemStack container, CallbackInfoReturnable<Boolean> cir) {
         if (!OxygenUtils.posHasOxygen(level, pos) || !ModUtils.isPlanet(level)) return;
         BlockState blockState = level.getBlockState(pos);
 
