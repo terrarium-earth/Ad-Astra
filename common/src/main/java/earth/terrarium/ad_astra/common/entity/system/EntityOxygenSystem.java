76a7658e63d4481a5a27227428fbd91522b99a8a
gisellevonbingen <gisellevonbingen@gmail.com>
2023-08-17 16:39:28 +0900

    Add EntityOxygenSystem.getOxygenStatus

--- common/src/main/java/earth/terrarium/ad_astra/common/entity/system/EntityOxygenSystem.java
+++ common/src/main/java/earth/terrarium/ad_astra/common/entity/system/EntityOxygenSystem.java
@@ -1,5 +1,7 @@
 package earth.terrarium.ad_astra.common.entity.system;
 
+import org.jetbrains.annotations.NotNull;
+
 import earth.terrarium.ad_astra.common.config.AdAstraConfig;
 import earth.terrarium.ad_astra.common.item.armor.SpaceSuit;
 import earth.terrarium.ad_astra.common.registry.ModDamageSource;
@@ -10,39 +12,70 @@ import earth.terrarium.botarium.api.fluid.FluidHooks;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.level.Level;
 
 public class EntityOxygenSystem {
-    public static void oxygenTick(LivingEntity entity, ServerLevel level) {
+    public enum EntityOxygenStatus {
+        /**
+         * AdAstraConfig.doOxygen is false or Entity is undead or<br>
+         * Entity has 'ad_astra:entities/lives_without_oxygen' tag or<br>
+         * Entity's level has oxygen
+         */
+        IGNORE,
+        /**
+         * Entity is equipped full set of space suit and<br>
+         * Entity should consume oxygen in equipped space suit
+         */
+        CONSUME,
+        /**
+         * Entity is not equipped space suit or No oxygen in space suit<br>
+         * Entity should take damage by suffocation
+         */
+        DAMAGE,
+    }
+
+    @NotNull
+    public static EntityOxygenStatus getOxygenStatus(LivingEntity entity, Level level) {
         if (!AdAstraConfig.doOxygen) {
-            return;
+            return EntityOxygenStatus.IGNORE;
         }
         if (entity.isInvertedHealAndHarm()) {
-            return;
+            return EntityOxygenStatus.IGNORE;
         }
 
         if (ModUtils.checkTag(entity, ModTags.LIVES_WITHOUT_OXYGEN)) {
-            return;
+            return EntityOxygenStatus.IGNORE;
         }
 
         if (OxygenUtils.levelHasOxygen(level) && !entity.isUnderWater()) {
-            return;
+            return EntityOxygenStatus.IGNORE;
         }
 
         boolean entityHasOxygen = OxygenUtils.entityHasOxygen(level, entity);
         boolean hasOxygenatedSpaceSuit = SpaceSuit.hasOxygenatedSpaceSuit(entity) && SpaceSuit.hasFullSet(entity);
 
         if (entityHasOxygen && hasOxygenatedSpaceSuit && entity.isUnderWater() && !entity.canBreatheUnderwater() && !entity.hasEffect(MobEffects.WATER_BREATHING)) {
-            consumeOxygen(entity);
-            return;
+            return EntityOxygenStatus.CONSUME;
         }
 
         if (!entityHasOxygen) {
             if (hasOxygenatedSpaceSuit) {
-                consumeOxygen(entity);
+                return EntityOxygenStatus.CONSUME;
             } else if (!ModUtils.armourIsOxygenated(entity)) {
-                entity.hurt(ModDamageSource.OXYGEN, AdAstraConfig.oxygenDamage);
-                entity.setAirSupply(-40);
+                return EntityOxygenStatus.DAMAGE;
             }
+        }
+
+        return EntityOxygenStatus.IGNORE;
+    }
+
+    public static void oxygenTick(LivingEntity entity, ServerLevel level) {
+        EntityOxygenStatus status = getOxygenStatus(entity, level);
+        if (status == EntityOxygenStatus.CONSUME) {
+            consumeOxygen(entity);
+        } else if (status == EntityOxygenStatus.DAMAGE) {
+            entity.hurt(ModDamageSource.OXYGEN, AdAstraConfig.oxygenDamage);
+            entity.setAirSupply(-40);
         }
     }
 
