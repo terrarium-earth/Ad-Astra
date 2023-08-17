36068d68e5829ebeb81555ad39c5ef8aa1a46da1
gisellevonbingen <gisellevonbingen@gmail.com>
2023-08-17 21:18:46 +0900

    MobMixin be extends LivingEntity

--- common/src/main/java/earth/terrarium/ad_astra/mixin/MobMixin.java
+++ common/src/main/java/earth/terrarium/ad_astra/mixin/MobMixin.java
@@ -8,20 +8,23 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import earth.terrarium.ad_astra.common.entity.system.EntityOxygenStatus;
 import earth.terrarium.ad_astra.common.entity.system.EntityOxygenSystem;
 import earth.terrarium.ad_astra.common.util.OxygenUtils;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelReader;
 
 @Mixin(Mob.class)
-public abstract class MobMixin {
+public abstract class MobMixin extends LivingEntity {
+
+    protected MobMixin(EntityType<? extends LivingEntity> entityType, Level level) {
+        super(entityType, level);
+    }
 
     @Inject(method = "checkSpawnObstruction", at = @At("HEAD"), cancellable = true)
     public void ad_astra$checkSpawnObstruction(LevelReader levelReader, CallbackInfoReturnable<Boolean> cir) {
-
         if (levelReader instanceof Level level && !OxygenUtils.levelHasOxygen(level)) {
-            Mob mob = (Mob) (Object) this;
-
-            if (EntityOxygenSystem.getOxygenStatus(mob, level) == EntityOxygenStatus.DAMAGE) {
+            if (EntityOxygenSystem.getOxygenStatus(this, level) == EntityOxygenStatus.DAMAGE) {
                 cir.setReturnValue(false);
             }
         }
