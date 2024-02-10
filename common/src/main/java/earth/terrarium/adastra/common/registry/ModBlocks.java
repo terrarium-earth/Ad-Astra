f84c8239340a1a4a87bb7da71dbf05c0e68e167d
Alex Nijjar <alexander.nijjar@icloud.com>
2024-02-10 16:54:26 -0600

    Fix fluid blocks

--- common/src/main/java/earth/terrarium/adastra/common/registry/ModBlocks.java
+++ common/src/main/java/earth/terrarium/adastra/common/registry/ModBlocks.java
@@ -116,6 +116,8 @@ public class ModBlocks {
     public static final RegistryEntry<Block> CRYO_FREEZER = MACHINES.register("cryo_freezer", () -> new MachineBlock(OSTRUM_PROPERTIES));
     public static final RegistryEntry<Block> OXYGEN_SENSOR = MACHINES.register("oxygen_sensor", () -> new DetectorBlock(OSTRUM_PROPERTIES));
 
+    public static final RegistryEntry<Block> OXYGEN = FLUIDS.register("oxygen", () -> new BotariumLiquidBlock(ModFluidProperties.OXYGEN, BlockBehaviour.Properties.copy(Blocks.WATER)));
+    public static final RegistryEntry<Block> HYDROGEN = FLUIDS.register("hydrogen", () -> new BotariumLiquidBlock(ModFluidProperties.HYDROGEN, BlockBehaviour.Properties.copy(Blocks.WATER)));
     public static final RegistryEntry<Block> OIL = FLUIDS.register("oil", () -> new BotariumLiquidBlock(ModFluidProperties.OIL, BlockBehaviour.Properties.copy(Blocks.WATER).mapColor(MapColor.COLOR_BLACK)));
     public static final RegistryEntry<Block> FUEL = FLUIDS.register("fuel", () -> new BotariumLiquidBlock(ModFluidProperties.FUEL, BlockBehaviour.Properties.copy(Blocks.WATER).mapColor(MapColor.COLOR_RED)));
     public static final RegistryEntry<Block> CRYO_FUEL = FLUIDS.register("cryo_fuel", () -> new CryoFuelLiquidBlock(ModFluidProperties.CRYO_FUEL, BlockBehaviour.Properties.copy(Blocks.WATER).mapColor(MapColor.COLOR_CYAN)));
