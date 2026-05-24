4e6ad2201bc3d98e2a0d7dc4646bc29dab985227
Mrbysco <Mrbysco@users.noreply.github.com>
2026-05-24 19:58:47 +0200

    Implement FabricItem for the fabric item extensions

--- src/fabric/java/earth/terrarium/adastra/common/items/ZipGunItemExtension.java
+++ src/fabric/java/earth/terrarium/adastra/common/items/ZipGunItemExtension.java
@@ -1,5 +1,6 @@
 package earth.terrarium.adastra.common.items;
 
+import net.fabricmc.fabric.api.item.v1.FabricItem;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
@@ -8,13 +9,14 @@ import net.msrandom.classextensions.ClassExtension;
 import net.msrandom.classextensions.ExtensionInject;
 
 @ClassExtension(ZipGunItem.class)
-public class ZipGunItemExtension extends Item{
+public class ZipGunItemExtension extends Item implements FabricItem {
 
     public ZipGunItemExtension(Item.Properties properties) {
         super(properties);
     }
 
     @ExtensionInject
+    @Override
     public boolean allowComponentsUpdateAnimation(Player player, InteractionHand hand, ItemStack oldStack, ItemStack newStack) {
         return false;
     }
