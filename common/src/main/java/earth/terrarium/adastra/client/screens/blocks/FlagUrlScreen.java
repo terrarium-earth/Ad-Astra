ffd4810d3d5e3c02a8ea8231f7d51bc7347f671b
AnotherPillow <85362273+AnotherPillow@users.noreply.github.com>
2026-06-17 19:21:46 +0100

    Change Regex to not require Imgur

--- common/src/main/java/earth/terrarium/adastra/client/screens/blocks/FlagUrlScreen.java
+++ common/src/main/java/earth/terrarium/adastra/client/screens/blocks/FlagUrlScreen.java
@@ -16,7 +16,7 @@ import java.util.regex.Pattern;
 
 public class FlagUrlScreen extends Screen {
 
-    private static final Pattern URL_REGEX = Pattern.compile("^https://i\\.imgur\\.com/(\\w+)\\.(png|jpeg|jpg|webp)$");
+    private static final Pattern URL_REGEX = Pattern.compile("^https://[\\w\\-.]+(?:/[\\w\\-._~:/?#\\[\\]@!$&'()*+,;=%]*)?\\.(png|jpe?g|webp|gif)$");
 
     private final BlockPos pos;
     private EditBox urlField;
